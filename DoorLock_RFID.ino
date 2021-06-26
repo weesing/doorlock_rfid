f1e14d1019d7adfd1bd62575b437d1246c738e3b
Yap Wee Sing <posttomichael@gmail.com>
2021-06-26 22:51:44 +0800

    Removed unecessary defines

--- DoorLock_RFID.ino
+++ DoorLock_RFID.ino
@@ -1,40 +1,36 @@
-#define RFID_ENABLED
-#define BUZZER_ENABLED
+#include <SPI.h>
+#include <MFRC522.h>
+#include <SoftwareSerial.h>
+  
+#define PIN_RFID_RST                          9
+#define PIN_RFID_SDA                          10
+#define PIN_BLE_RXD                           3
+#define PIN_BLE_TXD                           4
 
-#ifdef RFID_ENABLED
-  #include <SPI.h>
-  #include <MFRC522.h>
-  #include <SoftwareSerial.h>
-  
-  #define PIN_RFID_RST                          9
-  #define PIN_RFID_SDA                          10
-  #define PIN_BLE_RXD                           3
-  #define PIN_BLE_TXD                           4
-  
-  #define RFID_MS                               250
-  unsigned long rfidLastTime                    = 0;
-  
-  MFRC522 mfrc522(PIN_RFID_SDA, PIN_RFID_RST);   // Create MFRC522 instance.
-  
-  byte readCard[4];   // Stores scanned ID read from RFID Module
-  
-  SoftwareSerial btSerial(PIN_BLE_RXD, PIN_BLE_TXD); // RX | TX
-#endif
+#define RFID_MS                               250
+unsigned long rfidLastTime                    = 0;
 
-#ifdef BUZZER_ENABLED
-  #define PIN_BUZZER                            2
-  #define HIGH_SOUND                            3001
-  #define LOW_SOUND                             1000
-  #define SOUND_STEP                            500
-  #define SOUND_MS                              50
-  int soundToneTarget                           = 0;
-  int soundToneCurr                             = 0;
+MFRC522 mfrc522(PIN_RFID_SDA, PIN_RFID_RST);   // Create MFRC522 instance.
+
+byte readCard[4];   // Stores scanned ID read from RFID Module
+
+SoftwareSerial btSerial(PIN_BLE_RXD, PIN_BLE_TXD); // RX | TX
+
+#define PIN_BUZZER                            2
+#define HIGH_SOUND                            3001
+#define LOW_SOUND                             1000
+#define INIT_SOUND                            1000
+#define INIT_SOUND_MS                         1000
+#define RFID_INIT_SOUND                       2000
+#define RFID_INIT_MS                          1000
+#define SOUND_STEP                            500
+#define SOUND_MS                              50
+int soundToneTarget                           = 0;
+int soundToneCurr                             = 0;
   unsigned long soundToneLastTime;
-#endif
 
 unsigned long currTime;
 
-#ifdef BUZZER_ENABLED
 void scanSound() {
   tone(PIN_BUZZER, 2000, 100);
   delay(100);
@@ -79,9 +75,7 @@ void buzzerLoop() {
     }
   }
 }
-#endif
 
-#ifdef RFID_ENABLED
 void printBytes( byte toPrint[] ) {
   Serial.print("0x");
   for ( uint8_t i = 0; i < 4; i++ ) {
@@ -124,10 +118,8 @@ void rfidLoop() {
   if (tryReadRFID()) {
     Serial.println("Sending to master arduino... >> ");
     sendBLEData(readCard);
-#ifdef BUZZER_ENABLED
-//    scanSound();
+    
     lockSound();
-#endif
   }
 }
 
@@ -154,10 +146,9 @@ void bleLoop() {
 }
 
 void initBLE() {
-  btSerial.begin(115200);
-  delay(100);
   btSerial.println("U,9600,N");
   btSerial.begin(9600);  // HC-05 default speed in AT command more
+  delay(1000);
 }
 
 #endif
@@ -169,25 +160,19 @@ void setup() {
     Serial.println("Waiting for Serial port...");
   }
   Serial.println("Serial READY!\n");
+
+  pinMode(PIN_BUZZER, OUTPUT);
   
-#ifdef RFID_ENABLED
   initRFID();
   initBLE();
-#endif
-#ifdef BUZZER_ENABLED
-  pinMode(PIN_BUZZER, OUTPUT);
-  initSound();
-#endif
+
+  tone(PIN_BUZZER, INIT_SOUND_MS, INIT_SOUND);
 }
 
 void loop() {
   currTime = millis();
 
-#ifdef RFID_ENABLED
   rfidLoop();
   bleLoop();
-#endif
-#ifdef BUZZER_ENABLED
   buzzerLoop();
-#endif
 }
